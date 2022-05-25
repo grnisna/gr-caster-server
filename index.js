b782e6c2a8f76c20370734b0aa80fd9220c02024
nisanAhmed <mdnisanahmed63@gmail.com>
2022-05-25 16:11:22 +0600

    addmin role implement

--- index.js
+++ index.js
@@ -14,23 +14,23 @@ app.use(cors());
 app.use(express.json());
 
 // ---------- veryfy user function -------------- 
-const verifyUser = async(req,res,next) =>{
-    const authHeader  = req.headers.authorization;
-    
-    if(!authHeader){
-        res.status(401).send({message:'Un-authorized user'})
+const verifyUser = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        res.status(401).send({ message: 'Un-authorized user' })
     }
     const accessToken = authHeader.split(' ')[1];
-    jwt.verify(accessToken,process.env.USER_ACCESS_TOKEN,(err, decoded)=>{
-        if(err){
-            res.status(403).send({message:'Forbidden user'})
+    jwt.verify(accessToken, process.env.USER_ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            res.status(403).send({ message: 'Forbidden user' })
         }
         req.decoded = decoded;
 
         next();
     });
 
-    
+
 }
 
 //----------------- mongoDB connect ----------------
@@ -53,20 +53,7 @@ async function run() {
         const profileCollections = client.db("toolCollection").collection("profile");
 
 
-        // set user to mongoDB from client site -----------------
-        app.put('/user/:email', async(req,res)=>{
-            const email = req.params.email;
-            const user = req.body;
-            const filter = {email:email};
-            const options = { upsert: true };
-            const updateUser = {
-                $set:user
-            };
-            const result = await userCollections.updateOne(filter,updateUser,options);
-            const token = jwt.sign({email:email},process.env.USER_ACCESS_TOKEN,{expiresIn:'1d'});
 
-            res.send({result,token});
-        })
         // ---------- get tool data from DB ------- 
         app.get('/tool', async (req, res) => {
             const tools = await toolCollections.find().toArray();
@@ -86,7 +73,7 @@ async function run() {
         });
 
         //------------ set new review -----------------
-        app.post('/review',async(req,res)=>{
+        app.post('/review', async (req, res) => {
             const query = req.body;
             const result = await reviewCollections.insertOne(query);
             res.send(result);
@@ -100,17 +87,17 @@ async function run() {
         });
 
         // get booking data for client site --------------------
-        app.get('/booking',verifyUser, async (req, res) => {
+        app.get('/booking', verifyUser, async (req, res) => {
             const email = req.query.email;
             const decodedEmail = req.decoded.email;
-            
-            if(email === decodedEmail){
-                const filter = {email:email}
+
+            if (email === decodedEmail) {
+                const filter = { email: email }
                 const result = await bookingCollections.find(filter).toArray();
                 return res.send(result);
             }
-            else{
-               return res.status(403).send({message:'Forbidden User'})
+            else {
+                return res.status(403).send({ message: 'Forbidden User' })
             }
 
         });
@@ -129,61 +116,120 @@ async function run() {
         // get payment by user ----------------
         app.post('/create-payment-intent', async (req, res) => {
             const { total } = req.body;
-            const amount = total*100;
+            const amount = total * 100;
             const paymentIntent = await stripe.paymentIntents.create({
                 amount: amount,
                 currency: 'usd',
-                payment_method_types:['card'],
+                payment_method_types: ['card'],
             });
 
-            res.send({clientSecret: paymentIntent.client_secret});
+            res.send({ clientSecret: paymentIntent.client_secret });
         });
 
         //update payment info =================================
-        app.put('/booking/:id',async(req,res)=>{
+        app.put('/booking/:id', async (req, res) => {
             const id = req.params.id;
             const payment = req.body;
-            const filter = {_id:ObjectId(id)};
+            const filter = { _id: ObjectId(id) };
             const updateDoc = {
-                $set:{
-                    paid:true,
+                $set: {
+                    paid: true,
                     transactionId: payment.transactionId
                 }
             };
             const allPayment = await paymentCollections.insertOne(payment);
-            const updateBook = await bookingCollections.updateOne(filter,updateDoc);
+            const updateBook = await bookingCollections.updateOne(filter, updateDoc);
             res.send(updateDoc);
         });
 
         // cencel book item ------------------------ 
-        app.delete('/booking/:id', async(req,res)=>{
+        app.delete('/booking/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id:ObjectId(id)};
+            const filter = { _id: ObjectId(id) };
             const result = await bookingCollections.deleteOne(filter);
             res.send(result);
         });
 
-        // ------- upsert profile ------
-        app.post('/profile',verifyUser,async(req,res)=>{
-            
+        // ------- insert profile ------
+        app.post('/profile', verifyUser, async (req, res) => {
+
             const query = req.body;
-           
+
             const result = await profileCollections.insertOne(query);
             res.send(result);
         });
 
         // show up data client site --------------  
-        app.get('/profile',verifyUser, async(req,res)=>{
+        app.get('/profile', verifyUser, async (req, res) => {
             const email = req.query.email;
             const decodedEmail = req.decoded.email;
-            if( email === decodedEmail){
-                const filter = {email:email};
+            if (email === decodedEmail) {
+                const filter = { email: email };
                 const result = await profileCollections.findOne(filter);
                 return res.send(result);
-            }else{
-               return  res.status(403).send({message:'Forbidden Access'})
+            } else {
+                return res.status(403).send({ message: 'Forbidden Access' })
             }
+        });
+
+        //-------------------------- user section ---------------------------------------- 
+        //------------------------------------------------------------------ 
+
+        // --====================== normal user =========================
+
+        // set user to mongoDB from client site -----------------
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateUser = {
+                $set: user
+            };
+            const result = await userCollections.updateOne(filter, updateUser, options);
+            const token = jwt.sign({ email: email }, process.env.USER_ACCESS_TOKEN, { expiresIn: '1d' });
+
+            res.send({ result, token });
         })
+
+
+        // --====================== admin user =========================
+        // --====================== admin user =========================
+        // get all user for admin ----------------------- 
+        app.get('/admin', async (req, res) => {
+            const result = await userCollections.find().toArray();
+            res.send(result);
+        });
+        // set admin role -------------- 
+        app.put('/admin/:email', verifyUser, async (req, res) => {
+            const email = req.params.email;
+            const requesterEmail = req.decoded.email;
+            const runningRequester = await userCollections.findOne({ email: requesterEmail });
+            if (runningRequester === 'admin') {
+                const filter = { email: email };
+                const updateRole = {
+                    $set: {
+                        role: 'admin'
+                    }
+                };
+                const result = await userCollections.updateOne(filter, updateRole);
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden ' })
+            }
+        });
+
+        // showing route for admin ---------------
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollections.findOne({ email: email });
+            const findRole = user.role === 'admin';
+            res.send({ admin: findRole });
+
+        });
+
+
 
     }
     finally {
